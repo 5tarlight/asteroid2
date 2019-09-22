3075b4bb610d96d7672a8d9fb4c8d1a4aa53ca20
5tarlight <developerstarlight@gmail.com>
2019-09-22 20:34:57 +0900

    better embed

---
+++ src/lib/cmd/Embed.js
@@ -0,0 +1,11 @@
+import { RichEmbed } from 'discord.js'
+
+class Embed extends RichEmbed {
+  constructor() {
+    super()
+    super.setFooter('Starlight#7528 2019 © All rights reserved')
+    super.setTimestamp(new Date())
+  }
+}
+
+export default Embed
