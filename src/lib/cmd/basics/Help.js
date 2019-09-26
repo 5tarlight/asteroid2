4ab09e8995c801aa70b3ecf30f149461723338b9
5tarlight <developerstarlight@gmail.com>
2019-09-26 14:02:42 +0900

    better help command

--- src/lib/cmd/basics/Help.js
+++ src/lib/cmd/basics/Help.js
@@ -5,23 +5,53 @@ import { getCommands } from '../../../index'
 class Help extends CommandExecutor {
   run (bot, msg, args) {
     const commands = getCommands()
+    
+    if (args.length < 1) {
+      const categories = []
+
+      commands.forEach(command => {
+        if(!categories.includes(command.category)) {
+          categories.push(command.category)
+        }
+      })
+
+      const embed = new RichEmbed()
+        .setTitle('카테고리')
+        .setDescription('<@622743602292064277> 도움 <카테고리>')
+      
+      categories.forEach(c => {
+        embed.addField('**' + c + '**', '\u200B', true)
+      })
+
+      msg.channel.send(embed)
+      return
+    }
+
+    const category = args[0]
+    const helpCmds = []
+
+    commands.forEach(c => {
+      if(c.category === category) {
+        helpCmds.push(c)
+      }
+    })
 
     let embed = new RichEmbed()
       .setTitle('도움말')
 
     let i = 0
 
-    commands.forEach((command) => {
-      if(i > 20) {
+    helpCmds.forEach(hc => {
+      if (i > 20) {
         i = 0
         msg.channel.send(embed)
 
         embed = new RichEmbed()
           .setTitle('도움말 (연결)')
-        }
-        
-        embed.addField(command.cmd, command.description, true)
-        i++
+      }
+
+      embed.addField(`**${hc.cmd}**`, hc.description, true)
+      i++
     })
 
     msg.channel.send(embed)
